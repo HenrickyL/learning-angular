9a15ae0bbaf73b15c21715d3bf2e00c0bd183ddc
HenrickyL <Henrickyl1@gmail.com>
2025-08-20 20:22:38 -0300

    feat: event & conditional

--- 1-Saindo-do-ZERO/my-first-app/src/app/components/home/home.ts
+++ 1-Saindo-do-ZERO/my-first-app/src/app/components/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule, } from "@angular/common";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  name: string = 'Henricky';
+  onActive : boolean = false;
+  listItems: string[] = ["Fiddlesticks","Nami","Milio","Renata Glasc"]
 
+  onClick(event:any) {
+    this.onActive = !this.onActive;
+  }
 }
